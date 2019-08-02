0e7fa144f9b2f1c868c676d0559ea540faaf8179
zoff99 <zoff@zoff.cc>
2019-08-02 21:13:13 +0200

    add CI script

--- circle_scripts/toxproxy.sh
+++ circle_scripts/toxproxy.sh
@@ -1,4 +1,54 @@
 #! /bin/bash
 
-echo ping
+_HOME_="$(pwd)"
+export _HOME_
 
+cd "$1"
+_CTC_SRC_DIR_="$(pwd)"
+export _CTC_SRC_DIR_
+
+export _SRC_=$_HOME_/src/
+export _INST_=$_HOME_/inst/
+
+
+export CF2=" -O3 -g"
+export CF3=" "
+export VV1=" " # VERBOSE=1 V=1 "
+
+
+mkdir -p $_SRC_
+mkdir -p $_INST_
+
+export LD_LIBRARY_PATH=$_INST_/lib/
+export PKG_CONFIG_PATH=$_INST_/lib/pkgconfig
+
+cd "$_CTC_SRC_DIR_"/
+pwd
+ls -al
+
+
+./autogen.sh
+make clean
+export CFLAGS_=" $CF2 -D_GNU_SOURCE -I$_INST_/include/ -O3 -g -fstack-protector-all "
+export CFLAGS="$CFLAGS"
+export CFLAGS=" $CFLAGS -Werror=div-by-zero -Werror=format=2 -Werror=implicit-function-declaration "
+export LDFLAGS=-L$_INST_/lib
+
+./configure \
+--prefix=$_INST_ \
+--disable-soname-versions --disable-testing --disable-shared
+make -j$(nproc) || exit 1
+make install
+
+export CFLAGS=" $CFLAGS_ -fPIC "
+export CXXFLAGS=" $CFLAGS_ -fPIC "
+export LDFLAGS=" $LDFLAGS_ -fPIC "
+# timeout -k 242 240 make V=1 -j20 check || exit 0 # tests fail too often on CI -> don't error out on test failures
+
+
+
+# -------------- now compile toxproxy ----------------------
+
+echo "dummy"
+
+# -------------- now compile toxproxy ----------------------
