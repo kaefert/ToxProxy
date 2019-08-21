fafa29b1a2a4211c5965ac8e75d682fa0241b86b
kaefert <kaefert@N550JV>
2019-08-21 10:51:20 +0200

    circleCI install unzip for sqlite dependency build

--- circle_scripts/deps.sh
+++ circle_scripts/deps.sh
@@ -161,6 +161,7 @@ make install
 
 cd $_SRC_
 wget -O sqlite-amalgamation.zip https://www.sqlite.org/2019/sqlite-amalgamation-3290000.zip
+apt install -y --no-install-recommends unzip
 unzip -j sqlite-amalgamation.zip -d sqlite-amalgamation
 cd sqlite-amalgamation
 gcc shell.c sqlite3.c -lpthread -ldl -o $_INST_/lib/sqlite3.a
