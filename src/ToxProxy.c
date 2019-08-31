b429207d2b1d9a8aab885c9f919fca0a9e0ec006
Thomas Käfer <kaefert@gmail.com>
2019-08-31 07:48:19 +0200

    fix warning: Uninitialized variable passed to free

--- src/ToxProxy.c
+++ src/ToxProxy.c
@@ -508,7 +508,7 @@ Tox* openTox() {
 
 #ifdef USE_SEPARATE_SAVEDATA_FILE
 	FILE *f = fopen(savedata_filename, "rb");
-	uint8_t* savedata;
+	uint8_t* savedata = NULL;
 	if (f) {
 		fseek(f, 0, SEEK_END);
 		size_t savedataSize = ftell(f);
